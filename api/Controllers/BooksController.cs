afa465af2ddd06a36219eddfbc951eca923c6f4d
Fuheng Jiang <jiang.1574@osu.edu>
2019-04-02 17:46:38 -0400

    Lab 8

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Fisher.Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Api.Data;
 
 namespace Fisher.Bookstore.Api.Controllers
 {
@@ -112,7 +113,7 @@ namespace Fisher.Bookstore.Api.Controllers
             db.Books.Remove(book);
             db.SaveChanges();
 
-            return Nocontent();
+            return NoContent();
         }
     }
 }
