c0b679bb934a8dca420d3e77d2fb7d2dc4834978
Fuheng Jiang <jiang.1574@osu.edu>
2019-04-02 17:51:10 -0400

    lab 8

--- api/Models/Author.cs
+++ api/Models/Author.cs
@@ -9,5 +9,6 @@ namespace Fisher.Bookstore.Models
         public string Name { get; set; }
         public string Bio { get; set; }
         public List<Book> Titles { get; set; }
+        public
     }
 }
