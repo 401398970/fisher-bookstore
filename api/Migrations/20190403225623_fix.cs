37dcc12447fca516695ecb25934399df21065f35
Fuheng Jiang <jiang.1574@osu.edu>
2019-04-03 23:27:49 -0400

    lab 9

---
+++ api/Migrations/20190403225623_fix.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fisher.Bookstore.Api.Migrations
+{
+    public partial class fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "publicationDate",
+                table: "Books",
+                newName: "PublishDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishDate",
+                table: "Books",
+                newName: "publicationDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: true);
+        }
+    }
+}
