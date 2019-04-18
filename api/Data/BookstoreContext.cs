512b05e65d5047e1d849716f388660f0c1049461
Fuheng Jiang <jiang.1574@osu.edu>
2019-04-18 03:20:41 -0400

    lab 10

--- api/Data/BookstoreContext.cs
+++ api/Data/BookstoreContext.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Fisher.Bookstore.Models;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Fisher.Bookstore.Api.Models;
 
 namespace Fisher.Bookstore.Api.Data
 {
-    public class BookstoreContext : DbContext
+    public class BookstoreContext : IdentityDbContext<ApplicationUser>
     {
         public BookstoreContext(DbContextOptions<BookstoreContext> options)
             : base(options)
--- api/Startup.cs
+++ api/Startup.cs
@@ -13,6 +13,11 @@ using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Fisher.Bookstore.Models;
 using Fisher.Bookstore.Api.Data;
+using Fisher.Bookstore.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Fisher.Bookstore.Api
 {
@@ -31,6 +36,31 @@ namespace Fisher.Bookstore.Api
             services.AddDbContext<BookstoreContext>(options =>
         options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext")));
 
+            //Add this for identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<BookstoreContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(jwtOptions =>
+                {
+                    jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateActor = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = Configuration["JWTConfiguration:Issuer"],
+                        ValidAudience = Configuration["JWTConfiguration:Audience"],
+                        IssuerSigningKey = new
+            SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTConfiguration.Key"])
+            )
+                };
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -46,7 +76,8 @@ namespace Fisher.Bookstore.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // Add this for identity
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
